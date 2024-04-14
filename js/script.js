27165410de67aee1dd58cba6bbbc098e97f57f80
Ar4uk69 <126593592+ar4uk69@users.noreply.github.com>
2024-04-14 14:08:56 +0200

    starting ready

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let navLinks = document.getElementById("navLinks");
+let faBars = document.getElementById("faBars");
+let faTimes = document.getElementById("faTimes");
+function showMenu() {
+  navLinks.style.right = "0";
+  faBars.style.display = "none";
+  faTimes.style.display = "block";
+}
+function hideMenu() {
+  navLinks.style.right = "-40vw";
+  faTimes.style.display = "none";
+  faBars.style.display = "block";
+}
+/*---------To-do list ----------*/
