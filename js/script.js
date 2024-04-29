68052e7a9411b9e8368452d0175a465767d2a8b8
artemnchuk <artemnchuk@gmail.com>
2024-04-29 23:19:52 +0200

    fix some bugs & formating

--- js/script.js
+++ js/script.js
@@ -18,135 +18,54 @@ const toDoInput = document.getElementById("toDoInput");
 const toDoSection= document.getElementById("toDoSection");
 let OnlyList= document.createElement("div");
 OnlyList.className="OnlyList";
-toDoSection.appendChild(OnlyList);
 
-// function addUlBox() {
-//         for(let i = 0;i<4;i++){
-//         if (i==4) {
-//           alert("You already made 4 lists")
-//         } else {
-//           let div = document.createElement("div");
-//           let ul = document.createElement("ul");
-//           ul.innerHTML = ;
-//           listDiv.appendChild(ul);
-//           i++;
-//         }
-//       }
-//         inputBox.value = "";
-//         // saveData();
-//   }
-// function addTask() {
-//   if (inputBox.value === "") {
-//     alert("You must write something!");
-//   } else {
-//     let li = document.createElement("li");
-//     li.innerHTML = inputBox.value.charAt(0).toUpperCase()+inputBox.value.slice(1);
-//     listContainer.appendChild(li);
-//     let span = document.createElement("span");
-//     span.innerHTML= "\u00d7";
-//     li.appendChild(span);
-//   }
-//   inputBox.value = "";
-//   saveData();
-// }
-
-// listContainer.addEventListener("click",function(e){
-//     if(e.target.tagName==="LI"){
-//         e.target.classList.toggle("checked");
-//         saveData();
-//     }
-//     else if(e.target.tagName==="SPAN"){
-//         e.target.parentElement.remove();
-//         saveData();
-//     }
-// },false);
-// function saveData(){
-//     localStorage.setItem("data",listContainer.innerHTML);
-// }
-// function showList(){
-//     listContainer.innerHTML=localStorage.getItem("data");
-// }
-// showList();
-//List 2
-// listContainer2.addEventListener("click",function(e){
-//   if(e.target.tagName==="LI"){
-//       e.target.classList.toggle("checked");
-//       saveData2();
-//   }
-//   else if(e.target.tagName==="SPAN"){
-//       e.target.parentElement.remove();
-//       saveData2();
-//   }
-// },false);
-// function saveData2(){
-//   localStorage.setItem("data2",listContainer2.innerHTML);
-// }
-// function showList2(){
-//   listContainer2.innerHTML=localStorage.getItem("data2");
-// // }
-// showList2();
+let addButton = document.createElement('button');
+addButton.className="addButton";
+addButton.style.cssText = 'display:block;margin: 0 auto;'
+let i = document.createElement("i");
+addButton.appendChild(i);
+i.className="fa fa-folder-plus";
+i.style.cssText = 'color: white;'
+addButton.addEventListener('click', createToDoList);
+toDoSection.appendChild(addButton);
 
 function createToDoList() {
-  // Створюємо div елемент (контейнер)
   let divElement = document.createElement('div');
   divElement.className="listDiv";
 
-  // Створюємо input елемент
   let inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'text');
   inputElement.setAttribute('placeholder', 'Add a new task...');
   inputElement.className="inputBox";
-  
-  // Створюємо ul елемент
+
   let ulElement = document.createElement('ul');
   ulElement.className="listContainer";
 
-  // Додаємо input до div
   divElement.appendChild(inputElement);
-  
-  // Додаємо ul до div
   divElement.appendChild(ulElement);
-
-  // Додаємо div на сторінку
+  toDoSection.appendChild(OnlyList);
   OnlyList.appendChild(divElement);
+ 
 
-  // Додаємо обробник події для input
   inputElement.addEventListener('keypress', function(event) {
       if (event.key === 'Enter') {
           let task = inputElement.value.trim();
           if (task !== '') {
-              // Створюємо li елемент
               let liElement = document.createElement('li');
               liElement.textContent = task;
-
-              // Додаємо li до ul
               ulElement.appendChild(liElement);
-
-              // Очищаємо input
               inputElement.value = '';
           }
       }
   });
 }
 
-// Створюємо кнопку, яка викликає функцію createToDoList() при кліку
-let addButton = document.createElement('button');
-addButton.className="addButton";
-addButton.style.cssText = 'display:flex;font-size: 100px;'
-let i = document.createElement("i");
-addButton.appendChild(i);
-i.className="fa fa-folder-plus";
-i.style.cssText = 'color: white;'
-addButton.addEventListener('click', createToDoList);
-OnlyList.appendChild(addButton);
 
-
-
-// let docTitle = document.title;
-// let faviconImg = document.getElementById("favicon");
-// window.addEventListener("blur", () => {document.title = "Come back 👀";
-// faviconImg.setAttribute("href","/img/favicon2.png");
-// })
-// window.addEventListener("focus", () =>{document.title = docTitle;
-// faviconImg.setAttribute("href","/img/favicon.png");
-// })
+  let docTitle = document.title;
+  let faviconImg = document.getElementById("favicon");
+  window.addEventListener("blur", () => {document.title = "Come back 👀";
+  faviconImg.setAttribute("href","/img/favicon2.png");
+  })
+  window.addEventListener("focus", () =>{document.title = docTitle;
+  faviconImg.setAttribute("href","/img/favicon.png");
+  })
